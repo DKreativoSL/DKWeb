684227675a97ce7fbbc3593f183f9beaf3fb6a69
Pere Gomis <pgomismoreno@gmail.com>
2015-11-12 19:07:34 +0100

    Limpieza de codigo

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("cServidor", "localhost");
-define("cUsuario", "root");
-define("cPass","");
-define("cBd","dkweb_real");
-?>
--- dk-panel/conexion.php
+++ dk-panel/conexion.php
@@ -2,10 +2,10 @@
 
 	date_default_timezone_set('Europe/Madrid');
 
-	define("cServidor", "localhost");
-	define("cUsuario", "root");
-	define("cPass","");
-	define("cBd","mad");
+	define("cServidor", "SERVIDOR_BASE_DE_DATOS");
+	define("cUsuario", "USUARIO_BASE_DE_DATOS");
+	define("cPass","CONTRASEÑA_BASE_DE_DATOS");
+	define("cBd","NOMBRE_BASE_DE_DATOS");
 
 	//lanzo la conexión una vez
 	$conexion = mysqli_connect(cServidor, cUsuario, cPass, cBd);
